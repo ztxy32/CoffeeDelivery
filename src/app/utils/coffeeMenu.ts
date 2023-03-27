04f6be715d22cb6916c728b87a53d7495de44946
Erick E.C. Pereira <35979271+ztxy32@users.noreply.github.com>
2023-03-26 21:14:01 -0300

    Create coffeeMenu.ts

---
+++ src/app/utils/coffeeMenu.ts
@@ -0,0 +1,129 @@
+import TradicionalExpressCoffee from "./../assets/expresso_tradicional.png"
+import AmericanExpressCoffee from "./../assets/expresso_americano.png"
+import CreamExpressCoffee from "./../assets/expresso_cremoso.png"
+import IceExpressCoffee from "../assets/expresso_gelado.png"
+import CoffeeWithMilk from "../assets/cafe_com_leite.png"
+import Latte from "../assets/latte.png"
+import Capuccino from "../assets/capuccino.png"
+import Macchiato from "../assets/macchiato.png"
+import Mocaccino from "../assets/mocaccino.png"
+import HotChocolate from "../assets/chocolate_quente.png"
+import Cuban from "../assets/cubano.png"
+import Havaian from "../assets/havaiano.png"
+import Arabic from "../assets/arabe.png"
+import Irish from "../assets/irlandes.png"
+
+export const coffeeMenu = [{
+    coffeeLabel: ["TRADITIONAL"],
+    coffeeName: "Expresso Tradicional",
+    coffeeDescription: "O tradicional café feito com água quente e grãos moídos",
+    coffeePrice: 9.90,
+    coffeeId: 1,
+    coffeeImg: TradicionalExpressCoffee
+},
+{
+    coffeeLabel: ["TRADITIONAL"],
+    coffeeName: "Expresso Americano",
+    coffeeDescription: "Expresso diluído, menos intenso que o tradicional",
+    coffeePrice: 9.90,
+    coffeeId: 2,
+    coffeeImg: AmericanExpressCoffee
+},
+{
+    coffeeLabel: ["TRADITIONAL"],
+    coffeeName: "Expresso Cremoso",
+    coffeeDescription: "Café expresso tradicional com espuma cremosa",
+    coffeePrice: 9.90,
+    coffeeId: 3,
+    coffeeImg: CreamExpressCoffee
+},
+{
+    coffeeLabel: ["TRADITIONAL", "GELADO"],
+    coffeeName: "Expresso Gelado",
+    coffeeDescription: "Bebida preparada com café expresso e cubos de gelo",
+    coffeePrice: 9.90,
+    coffeeId: 4,
+    coffeeImg: IceExpressCoffee
+},
+{
+    coffeeLabel: ["TRADITIONAL", "COM LEITE"],
+    coffeeName: "Café com Leite",
+    coffeeDescription: "Meio a meio de expresso tradicional com leite vaporizado",
+    coffeePrice: 9.90,
+    coffeeId: 5,
+    coffeeImg: CoffeeWithMilk
+},
+{
+    coffeeLabel: ["TRADITIONAL", "COM LEITE"],
+    coffeeName: "Latte",
+    coffeeDescription: "Uma dose de café expresso com o dobro de leite e espuma cremosa",
+    coffeePrice: 9.90,
+    coffeeId: 6,
+    coffeeImg: Latte
+},
+{
+    coffeeLabel: ["TRADITIONAL", "COM LEITE"],
+    coffeeName: "Capuccino",
+    coffeeDescription: "Bebida com canela feita de doses iguais de café, leite e espuma",
+    coffeePrice: 9.90,
+    coffeeId: 7,
+    coffeeImg: Capuccino
+},
+{
+    coffeeLabel: ["TRADITIONAL", "COM LEITE"],
+    coffeeName: "Macchiato",
+    coffeeDescription: "Café expresso misturado com um pouco de leite quente e espuma",
+    coffeePrice: 9.90,
+    coffeeId: 8,
+    coffeeImg: Macchiato
+},
+{
+    coffeeLabel: ["TRADITIONAL", "COM LEITE"],
+    coffeeName: "Mocaccino",
+    coffeeDescription: "Café expresso com calda de chocolate, pouco leite e espuma",
+    coffeePrice: 9.90,
+    coffeeId: 9,
+    coffeeImg: Mocaccino
+},
+{
+    coffeeLabel: ["ESPECIAL", "COM LEITE"],
+    coffeeName: "Chocolate Quente",
+    coffeeDescription: "Bebida feita com chocolate dissolvido no leite quente e café",
+    coffeePrice: 9.90,
+    coffeeId: 10,
+    coffeeImg: HotChocolate
+},
+{
+    coffeeLabel: ["ESPECIAL", "ALCOÓLICO", "GELADO"],
+    coffeeName: "Cubano",
+    coffeeDescription: "Drink gelado de café expresso com rum, creme de leite e hortelã",
+    coffeePrice: 9.90,
+    coffeeId: 11,
+    coffeeImg: Cuban
+},
+{
+    coffeeLabel: ["ESPECIAL"],
+    coffeeName: "Havaiano",
+    coffeeDescription: "Bebida adocicada preparada com café e leite de coco",
+    coffeePrice: 9.90,
+    coffeeId: 12,
+    coffeeImg: Havaian
+},
+{
+    coffeeLabel: ["ESPECIAL"],
+    coffeeName: "Árabe",
+    coffeeDescription: "Bebida preparada com grãos de café árabe e especiarias",
+    coffeePrice: 9.90,
+    coffeeId: 13,
+    coffeeImg: Arabic
+},
+{
+    coffeeLabel: ["ESPECIAL", "ALCOÓLICO"],
+    coffeeName: "Irlandês",
+    coffeeDescription: "Bebida a base de café, uísque irlandês, açúcar e chantilly",
+    coffeePrice: 9.90,
+    coffeeId: 14,
+    coffeeImg: Irish
+}
+
+]
