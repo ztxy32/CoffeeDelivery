fb78969905ae2a903c141a999655d112622bb405
Erick E.C. Pereira <35979271+ztxy32@users.noreply.github.com>
2023-03-27 20:16:07 -0300

    updated header font

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -2,6 +2,8 @@
 import Image from "next/image"
 import Logo from "../assets/Logo.png"
 import { MapPin, ShoppingCart } from "phosphor-react"
+import { Roboto } from 'next/font/google'
+const roboto = Roboto({ subsets: ['latin'], weight: ["400"]})
 
 export function Header(){
     return(
@@ -9,7 +11,7 @@ export function Header(){
             <Image src={Logo} alt="" className="w-[85px] h-[40px]" />
             <div className="flex gap-3">
                 <span 
-                    className="flex center justify-center bg-purple-light text-purple-dark p-2 h-[38px] rounded-md"
+                    className={`${roboto.className} flex items-center justify-center bg-purple-light text-purple-dark p-2 h-[38px] rounded-md`}
                 >
                     <MapPin 
                         color="#8047F8" size={18} 
