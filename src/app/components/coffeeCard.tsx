2efc46c34b837a9fa18bb17091f0632245b04d28
Erick E.C. Pereira <35979271+ztxy32@users.noreply.github.com>
2023-03-26 21:14:12 -0300

    Update coffeeCard.tsx

--- src/app/components/coffeeCard.tsx
+++ src/app/components/coffeeCard.tsx
@@ -1,27 +1,50 @@
 import { ShoppingCartSimple } from "phosphor-react"
 import { Roboto, Baloo_2 } from 'next/font/google'
 import Image from "next/image"
-import ExpressCoffee from "../assets/expresso_tradicional.png"
-import { Counter } from "./Counter"
+import { Counter } from "./counter"
 
 const roboto = Roboto({ subsets: ['latin'], weight: ["400", "700"]})
 const baloo_2 = Baloo_2({ subsets: ['latin'], weight: ["700", "800"]})
 
-export function CoffeeCard(){
+interface coffeeCardProps{
+    coffeeLabel: string[]
+    coffeeName: string
+    coffeeDescription: string
+    coffeePrice: number
+    coffeeId: number
+    coffeeImg?: string
+    //buyCoffee: (coffeePrice: string, coffeeQuantity: string) => void
+}
+
+export function CoffeeCard({
+    coffeeDescription, 
+    coffeeImg, 
+    coffeeLabel, 
+    coffeeName, 
+    coffeePrice, 
+}: coffeeCardProps){
     return(
         <div 
             className="bg-base-card w-[256px] h-[310px] rounded-tl-md rounded-tr-[36px] rounded-br-md rounded-bl-[36px] flex flex-col items-center justify-center"
         >
-            <Image className="mt-[-40px] mb-3" src={ExpressCoffee} alt=""/>
-            <span 
-                className={`${roboto.className} bg-yellow-light py-1 px-2 text-yellow-dark mb-5 h-[21px] rounded-[100px] flex items-center justify-center`}
-            >
-                TRADICIONAL
+            <span className="mt-[-60px]">
+                <Image src={coffeeImg} alt={coffeeDescription}/>
             </span>
-            <p  className={`${baloo_2.className} text-base-subtitle text-xl font-extrabold`}>Expresso Tradicional</p>
-            <p className={`${roboto.className} px-5 text-base-label text-sm mb-[33px]`}>O tradicional café feito com água quente e grãos moídos</p>
+            <div className="flex items-center justify-center text-[10px]">
+                {coffeeLabel.map(item => 
+                    <span 
+                        key={item}
+                        className={`${roboto.className} bg-yellow-light py-1 px-2 text-yellow-dark mb-5 mr-1 h-[21px] rounded-[100px]`}
+                    >
+                        {item}
+                    </span>    
+                )}
+            </div>
+
+            <p  className={`${baloo_2.className} text-base-subtitle text-xl font-extrabold`}>{coffeeName}</p>
+            <p className={`${roboto.className} px-5 text-base-label text-sm mb-[33px]`}>{coffeeDescription}</p>
             <div className="flex flex-row items-center">
-                <p className={`${baloo_2.className} text-2xl text-base-text mr-[23px]`}>9,90</p>
+                <p className={`${baloo_2.className} text-2xl text-base-text mr-[23px]`}>{coffeePrice}</p>
 
                 <Counter/>
 
